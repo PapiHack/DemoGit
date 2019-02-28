b500da1885ad896bc6885b9f850bce1dbdad4360
P@piH@ck3R <mssmbaye@gmail.com>
2019-02-28 16:30:30 +0000

    ajout de la figure rectangle.php

---
+++ Rectangle.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* @author PapiHack
+* @since 28/02/19
+*
+* Classe représentant un cercle.
+*/
+class Rectangle implements Figure
+{
+  public function jeMeDecris()
+  {
+    echo "Je me présentes, je suis un rectangle ! <br/>";
+  }
+}
