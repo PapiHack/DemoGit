878e0cbf8de3d4bad5eb0532d6469706e0816beb
P@piH@ck3R <mssmbaye@gmail.com>
2019-02-28 16:28:48 +0000

    ajout du carre.php :)

---
+++ Carre.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* @author PapiHack
+* @since 28/02/19
+*
+* Classe représentant un cercle.
+*/
+class Carre implements Figure
+{
+  public function jeMeDecris()
+  {
+    echo "Je me présentes, je suis un carrée ! <br/>";
+  }
+}
